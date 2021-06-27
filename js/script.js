a0a1031320db90fa871fbcaef7e5c9f7ed132460
Arcaino <82480321+Arcaino@users.noreply.github.com>
2021-06-27 17:47:40 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var slides = document.querySelectorAll('.slider__box__slide');
+var btns = document.querySelectorAll('.navigation__btn');
+let currentSlide = 1;
+
+var manualNav = function(manual){
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+            btn.classList.remove('active');
+        });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+    });
+});
