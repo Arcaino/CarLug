175cdd9e67c9da078509b975ee5422578c894c16
Arcaino <82480321+Arcaino@users.noreply.github.com>
2021-06-28 10:20:52 -0300

    inclusão do swiper

--- js/script.js
+++ js/script.js
@@ -1,23 +1,12 @@
-var slides = document.querySelectorAll('.slider__box__slide');
-var btns = document.querySelectorAll('.navigation__btn');
-let currentSlide = 1;
-
-var manualNav = function(manual){
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-
-        btns.forEach((btn) => {
-            btn.classList.remove('active');
-        });
-    });
-
-    slides[manual].classList.add('active');
-    btns[manual].classList.add('active');
-}
-
-btns.forEach((btn, i) => {
-    btn.addEventListener("click", () => {
-        manualNav(i);
-        currentSlide = i;
-    });
+var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
 });
